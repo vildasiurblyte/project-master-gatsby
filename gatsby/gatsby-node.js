274f716a669f915b11d4ac7cec81d5e1ca3e7101
vildasiurblyte <vilsiu@gmail.com>
2020-10-09 22:36:16 +0200

    feat: added gatsby node config and pizza template

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,31 @@
+import path from 'path';
+
+const turnPizzasIntoPages = async ({ graphql, actions }) => {
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+};
+
+export const createPages = async (params) => {
+  await turnPizzasIntoPages(params);
+};
